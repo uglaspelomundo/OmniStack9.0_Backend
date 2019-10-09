2cf7329ceeb45061a98fbae18f483af680713aac
Douglas Santos Pereira <douglas.pereira@dougnoMacBook-puro.local>
2019-10-09 20:30:45 -0300

    Estudo

--- src/shared/server.js
+++ src/shared/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 
 const app = express();
 
 //mongoose.connect('mongodb+srv://teste:teste123@omnistack-n3ytn.mongodb.net/mydatabase?retryWrites=true&w=majority', {
-    mongoose.connect('mongodb://localhost:27017/princedb', {
+mongoose.connect('mongodb://localhost:27017/princedb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).catch(error => console.log(error))
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
